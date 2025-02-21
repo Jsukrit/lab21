a395f217da9f962dce9e8defdaa828a8588823de
Jsukrit <sukritdn1@gmail.com>
2025-02-21 17:21:31 +0700

    Update average_finished.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
 
+using namespace std;
+
+int main(int  argc,char *argv[]){
+    int count=0,sum = 0,num = 0;
+    double average;
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }
+    else{
+    for(int i = 0 ; i < argc-1 ;i++){
+        num = atoi(argv[i+1]);
+        sum = sum + num;
+        count ++;
+    }
+    average = sum/count;
+    cout << "---------------------------------"<< endl;
+    cout << "Average of " << count << " numbers = " << average << endl ;
+    cout << "---------------------------------";
+    }    
+}
